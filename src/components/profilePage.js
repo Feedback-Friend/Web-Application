56f67603ed9e81338fe8076bef16f819a266f8cb
deepSpaceMonkey <ennis.salam@protonmail.com>
2022-09-28 14:52:16 -0500

    Testing grid in material ui

--- src/components/profilePage.js
+++ src/components/profilePage.js
@@ -7,12 +7,35 @@ import Nav from './nav';
 
 function ProfilePage(props){
     const {userID} = props
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
 
+    firstName.setUsername
+    
     useEffect(() => {
         // res = fetch()
     }
     )
-return(<div></div>)
+    
+    return(      
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={6}>
+            <Item>1</Item>
+        </Grid>
+        <Grid item xs={6}>
+            <Item>2</Item>
+        </Grid>
+        <Grid item xs={6}>
+            <Item>3</Item>
+        </Grid>
+        <Grid item xs={6}>
+            <Item>4</Item>
+        </Grid>
+        </Grid>
+    )
 } 
 
 // Makes the function exportable
