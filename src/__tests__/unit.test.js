32a2929392689a4061fc80d642f7bb789f88c08a
bennettburks <60748003+bennettburks@users.noreply.github.com>
2022-10-30 19:12:13 -0500

    Add first login page test

--- src/__tests__/unit.test.js
+++ src/__tests__/unit.test.js
@@ -1,5 +1,21 @@
-import '@testing-library/jest-dom'
+import React from 'react';
+import '@testing-library/jest-dom';
+import { fireEvent, getByText, render } from '@testing-library/react';
+import LoginPage from '../components/loginPage';
 
-test('example test', () => {
-    expect(true).toBe(true);
+test('login page correct submission', async() => {
+    // mock submit function in loginPage
+    const submit = jest.fn();
+
+    // render page
+    const { getByLabelText, getByText } = render(<LoginPage setUserID={() => {}} />);
+    const username = getByLabelText(/Username/i); //searches for substring 'username' ignoring case
+    const password = getByLabelText(/Password/i);
+
+    // enter values in username/password field, and submit
+    fireEvent.change(username, { target: { value: "jh" } });
+    expect(username.value).toBe("jh");
+    fireEvent.change(password, { target: { value: "password!!!" } });
+    expect(password.value).toBe("password!!!");
+    fireEvent.click(getByText(/Submit/i));
 });
