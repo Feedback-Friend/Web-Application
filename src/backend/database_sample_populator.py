2a68aae008cb5038422b2df280d4885140a9af98
Anika-Singh <anikasingh2001@gmail.com>
2022-10-31 21:13:03 -0500

    user test cases

--- src/backend/database_sample_populator.py
+++ src/backend/database_sample_populator.py
@@ -14,7 +14,97 @@ def populateTestUsers(cursor):
     registerUser(cursor, "Billy", "Bo", "bb", "password!", "bb@yahoo.com") #3
     registerUser(cursor, "Jack", "Hills", "jh", "password!!!", "jh@yahoo.com") #4
     registerUser(cursor, "Hugo", "Lee", "hlee", "pass", "h@yahoo.com") #5
+    testUsers(cursor)
 
+def testUsers(cursor):
+    testgetFirstName(cursor)
+    testupdateFirstName(cursor)
+    testgetLastName(cursor)
+    testupdateLastName(cursor)
+    testgetUserName(cursor)
+    testgetPassWord(cursor)
+    testupdatePassWord(cursor)
+    testgetEmailAddress(cursor)
+    testupdateEmailAddress(cursor)
+    testdeleteUser(cursor)
+    testgetUserInfo(cursor)
+
+def testgetFirstName(cursor):
+    assert getFirstName(cursor, 1) == "Kevin"
+    assert getFirstName(cursor, 2) == "Bob"
+    assert getFirstName(cursor, 3) == "Billy"
+    assert getFirstName(cursor, 4) == "Jack"
+    assert getFirstName(cursor, 5) == "Hugo"
+
+def testupdateFirstName(cursor):
+    updateFirstName(cursor, 2, "Bob2")
+    assert getFirstName(cursor, 2) == "Bob2"
+    updateFirstName(cursor, 2, "Bob")
+    assert getFirstName(cursor, 2) == "Bob"
+
+def testgetLastName(cursor):
+    assert getLastName(cursor, 1) == "Li"
+    assert getLastName(cursor, 2) == "James"
+    assert getLastName(cursor, 3) == "Bo"
+    assert getLastName(cursor, 4) == "Hills"
+    assert getLastName(cursor, 5) == "Lee"
+
+def testupdateLastName(cursor):
+    updateLastName(cursor, 2, "James2")
+    assert getLastName(cursor, 2) == "James2"
+    updateLastName(cursor, 2, "James")
+    assert getLastName(cursor, 2) == "James"
+
+def testgetUserName(cursor):
+    assert getUserName(cursor, 1) == "kevinli22527"
+    assert getUserName(cursor, 2) == "bj"
+    assert getUserName(cursor, 3) == "bb"
+    assert getUserName(cursor, 4) == "jh"
+    assert getUserName(cursor, 5) == "hlee"
+
+def testupdateUserName(cursor):
+    updateUserName(cursor, 2, "bj2")
+    assert getUserName(cursor, 2) == "bj2"
+    updateUserName(cursor, 2, "bj")
+    assert getUserName(cursor, 2) == "bj"  
+
+def testgetPassWord(cursor):
+    assert getPassWord(cursor, 1) == "kli1"
+    assert getPassWord(cursor, 2) == "password"
+    assert getPassWord(cursor, 3) == "password!"
+    assert getPassWord(cursor, 4) == "password!!!"
+    assert getPassWord(cursor, 5) == "pass"
+
+def testupdatePassWord(cursor):
+    updatePassWord(cursor, 2, "password1")
+    assert getPassWord(cursor, 2) == "password1"
+    updatePassWord(cursor, 2, "password")
+    assert getPassWord(cursor, 2) == "password"  
+
+def testgetEmailAddress(cursor):
+    assert getEmailAddress(cursor, 1) == "kevinli22527@yahoo.com"
+    assert getEmailAddress(cursor, 2) == "bj@yahoo.com"
+    assert getEmailAddress(cursor, 3) == "bb@yahoo.com"
+    assert getEmailAddress(cursor, 4) == "jh@yahoo.com"
+    assert getEmailAddress(cursor, 5) == "h@yahoo.com"
+
+def testupdateEmailAddress(cursor):
+    updateEmailAddress(cursor, 2, "j@yahoo.com")
+    assert getEmailAddress(cursor, 2) == "j@yahoo.com"
+    updateEmailAddress(cursor, 2, "bj@yahoo.com")
+    assert getEmailAddress(cursor, 2) == "bj@yahoo.com"  
+
+def testdeleteUser(cursor):
+    deleteUser(cursor, 5)
+    assert getEmailAddress(cursor, 5) == -1
+    assert getFirstName(cursor, 5) == -1
+
+def testgetUserInfo(cursor):
+    assert getUserInfo(cursor, 2)["firstName"] == "Bob"
+    assert getUserInfo(cursor, 2)["lastName"] == "James"
+    assert getUserInfo(cursor, 2)["username"] == "bj"
+    assert getUserInfo(cursor, 2)["password"] == "password"
+    assert getUserInfo(cursor, 2)["email"] == "bj@yahoo.com"
 
 """
 Adds test contact lists to the SQL database
